180844874df15a5b56c1a4ea262cdf527e1b8873
Anishsir404 <anishji0306@gmail.com>
2022-08-18 08:33:32 +0545

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,44 @@
+const Clarifai = require('clarifai');
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+ apiKey: '088910272e2540008426a654b2b1eb93'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+    // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+    // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+    // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+    // is to use a different version of their model that works like the ones found here: https://github.com/Clarifai/clarifai-javascript/blob/master/src/index.js
+    // so you would change from:
+    // .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    // to:
+    // .predict('53e1df302c079b3db8a0a36033ed2d15', req.body.input)
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+    // entries[0] --> this used to return the entries
+    // TO
+    // entries[0].entries --> this now returns the entries
+    res.json(entries[0].entries);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall
+}
